df16dc02d0197cd17bccd07c4516fbab344fb1e8
Jerry <jerry.tamlander@helsinki.fi>
2017-12-29 03:14:10 +0200

    testing to check if not in production

--- models/person.js
+++ models/person.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+
+
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
 
 const url = process.env.MONGODB_URI
 
